391d16a16f1c155d9b3320b49b8034b4baab3b10
Andrei Yechkalov <echkalov@ukr.net>
2017-07-10 19:18:10 +0300

    news

---
+++ www/app/code/Brew/News/Block/News.php
@@ -0,0 +1,10 @@
+<?php
+namespace Brew\News\Block;
+
+class News extends \Magento\Framework\View\Element\Template
+{
+    public function getNewsTxt()
+    {
+        return 'Some news will be here';
+    }
+}
